da3572e1d477d9ae07e32ca363a2de30b8dbc880
roi <raseminiano00@gmail.com>
2024-03-17 11:10:44 +0800

    - Import fontawesome for icons - Import tailwind css to hasten development - Creation of navbar content and styling

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "navy-blue": "#141E61",
+      },
+    },
+    fontFamily: {
+      sans: ["Comic Sans MS", "sans-serif"],
+    },
+  },
+  variants: {},
+  plugins: [],
+};
